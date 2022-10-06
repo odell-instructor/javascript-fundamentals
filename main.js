51d55a557b1b93431cf194f3766956db16ccd0f2
Roger O'Dell <odell.instructor@gmail.com>
2022-10-06 15:33:58 -0500

    Lesson 2 - My First Application

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('Hello World');
+
+// Single Line Comment
+
+/** Multi-line
+ * Comments
+ */
+
+let variableName = 'Hello';
+
+console.log(variableName);
+
+variableName = 10;
+
+console.log(variableName);
