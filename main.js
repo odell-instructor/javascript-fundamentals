b2a346be4fa95b6207a7806ec49532db06cd23ed
Roger O'Dell <odell.instructor@gmail.com>
2026-01-28 18:00:33 -0600

    Lesson 2 My first file

---
+++ main.js
@@ -0,0 +1,17 @@
+// My first set of code
+console.log('Hello JavaScript');
+
+/* Below will be my first variables */
+let userName = "Bob";
+const PI = 3.14;
+
+// PI = 3.25;   Cannot change the value of a constant
+//userName = "Joe";
+//userName = 42;
+
+console.log(userName);
+console.log(PI);
+
+let something;
+something = 'something';
+console.log(something);
---
+++ terms/lesson1.md
@@ -0,0 +1,5 @@
+# Lesson 1 - Introduction to JavaScript
+
+### Code Comments
+- // for a single comment
+- /* comment */ for block or multi-line comment
